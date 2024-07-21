93a1dc73960f411239b862c04ce2b23b2e82e3d2
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-07-22 00:51:12 +0100

    Remove babel warning and test formatting

--- wordy/eslint.config.mjs
+++ wordy/eslint.config.mjs
@@ -6,22 +6,26 @@ import js from "@eslint/js";
 export default [
 
   // eslint default entries
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   js.configs.recommended,
   pluginReactConfig,
 
   // My own entries to ignore eslint errors for brand new React project
-  {languageOptions: { globals: globals.jest }},
-  {settings: {
-    "react": {
-      "version": "detect"
+  { languageOptions: { globals: globals.jest } },
+  {
+    settings: {
+      "react": {
+        "version": "detect"
+      }
     }
-  }},
-  {rules: { 
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-uses-react": 0
-  }}
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-uses-react": 0
+    }
+  }
 ];
