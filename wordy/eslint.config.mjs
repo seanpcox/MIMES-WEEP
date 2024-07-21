797700ded188abab3f40710607bff40451009067
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-07-22 00:43:05 +0100

    First commit, default React App created with npx create-react-app. Added eslint functionality and updated eslint config file to ignore errors thrown on default created code.

---
+++ wordy/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import js from "@eslint/js";
+
+export default [
+
+  // eslint default entries
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  js.configs.recommended,
+  pluginReactConfig,
+
+  // My own entries to ignore eslint errors for brand new React project
+  {languageOptions: { globals: globals.jest }},
+  {settings: {
+    "react": {
+      "version": "detect"
+    }
+  }},
+  {rules: { 
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-uses-react": 0
+  }}
+];
