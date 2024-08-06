28c6c92de1af6b7cf5cb41e9d5f0158b38c74c4f
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-08-07 00:51:50 +0100

    Update for ios context menu workaround

--- mimesWeep/src/iosContextMenuHandler.js
+++ mimesWeep/src/iosContextMenuHandler.js
@@ -17,20 +17,25 @@ export default class ContextMenuHandler {
             this.contextMenuPossible = false;
             this.callback(touch);
         }, longPressDuration);
+
+        e.preventDefault();
     };
 
     onTouchMove = e => {
         clearTimeout(this.longPressCountdown);
+        e.preventDefault();
     };
 
     onTouchCancel = e => {
         this.contextMenuPossible = false;
         clearTimeout(this.longPressCountdown);
+        e.preventDefault();
     };
 
     onTouchEnd = e => {
         this.contextMenuPossible = false;
         clearTimeout(this.longPressCountdown);
+        e.preventDefault();
     };
 
     onContextMenu = e => {
