6df94a320c9e3a533c97a17372bf23a877653d5e
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-09-15 15:06:37 +0100

    Some name changes

--- mimesWeep/src/components/board.js
+++ mimesWeep/src/components/board.js
@@ -24,7 +24,8 @@ const Board = forwardRef(function Board(props, inputRef) {
 
     // REFS
 
-    var ref = useRef(null);
+    // Holds all board squares in a 1D array format for reference
+    var squaresRef = useRef(null);
 
     const pushRef = (square) => {
         // On render all board square components will be pushed into 1D array tempRef
@@ -34,6 +35,10 @@ const Board = forwardRef(function Board(props, inputRef) {
             tempRef.push(square);
         }
     };
+
+    // Do we wish to perform auto chording action when a number square is revealed by a click
+    // Verse the user manually having to click the just revealed number square to perform chording (normal mode)
+    var autoChordOnReveal = useRef(false);
 
 
     // HANDLER
@@ -75,11 +80,11 @@ const Board = forwardRef(function Board(props, inputRef) {
     // Effect to reference all of our board squares in a 1D array
     useEffect(() => {
         // After render we update our actual ref object to match tempRef
-        ref.current = tempRef;
+        squaresRef.current = tempRef;
 
         return () => {
             // On unmount we reset the refs so they are empty for next render
-            ref.current = null;
+            squaresRef.current = null;
         }
     });
 
@@ -105,6 +110,8 @@ const Board = forwardRef(function Board(props, inputRef) {
      */
     function btnLeftClickCallback(indexI, indexJ) {
 
+        console.log("We're clicking");
+
         // If the square is flagged then return, flag must be removed before it can be reveraled
         if (array[indexI][indexJ] >= 9) {
             return;
@@ -123,7 +130,7 @@ const Board = forwardRef(function Board(props, inputRef) {
             props.lostGameCallback();
 
             // Call the mime revealed function on the board square
-            ref.current[getRefIndex(width, indexI, indexJ)].mimeRevealed();
+            squaresRef.current[getRefIndex(width, indexI, indexJ)].mimeRevealed();
 
             // Exit function
             return;
@@ -154,12 +161,12 @@ const Board = forwardRef(function Board(props, inputRef) {
                 var refIndex = getRefIndex(width, zeroNeighbors[n][0], zeroNeighbors[n][1]);
 
                 // Call the refresh function on the board square component
-                ref.current[refIndex].refresh(array[zeroNeighbors[n][0]][zeroNeighbors[n][1]]);
+                squaresRef.current[refIndex].refresh(array[zeroNeighbors[n][0]][zeroNeighbors[n][1]]);
             }
         }
 
         // Refresh the initially clicked on square component
-        ref.current[getRefIndex(width, indexI, indexJ)].refresh(array[indexI][indexJ]);
+        squaresRef.current[getRefIndex(width, indexI, indexJ)].refresh(array[indexI][indexJ]);
     }
 
     /**
@@ -196,7 +203,7 @@ const Board = forwardRef(function Board(props, inputRef) {
         }
 
         // Refresh the board square component selected
-        ref.current[getRefIndex(width, indexI, indexJ)].refresh(array[indexI][indexJ]);
+        squaresRef.current[getRefIndex(width, indexI, indexJ)].refresh(array[indexI][indexJ]);
     }
 
     /**
@@ -216,14 +223,19 @@ const Board = forwardRef(function Board(props, inputRef) {
      */
     function btnChordActionCallback(indexI, indexJ, isReveal, isHighlight = false) {
 
+        if (isReveal) {
+            console.log("We're chording");
+        }
+
         // Get the coordinates of squares to be revealed as part of the chord action
-        var revealNeighborsCoords = logic.getChordActionNeighbors(array, indexI, indexJ);
+        var rNgCoords = logic.getChordActionNeighbors(array, indexI, indexJ);
 
         // If we have neighbors to reveal
-        if (revealNeighborsCoords && revealNeighborsCoords.length > 0 && isReveal) {
+        if (rNgCoords && rNgCoords.length > 0 && isReveal) {
+
             // Perform left click action on all of the neighbors we wish to reveal
-            for (let index = 0; index < revealNeighborsCoords.length; index++) {
-                btnLeftClickCallback(revealNeighborsCoords[index][0], revealNeighborsCoords[index][1]);
+            for (let index = 0; index < rNgCoords.length; index++) {
+                btnLeftClickCallback(rNgCoords[index][0], rNgCoords[index][1]);
             }
 
             // Exit
@@ -235,7 +247,7 @@ const Board = forwardRef(function Board(props, inputRef) {
 
         // Perform highlight on all of the unrevealed and unflagged neighbors
         for (let index = 0; index < hNgCoords.length; index++) {
-            ref.current[getRefIndex(width, hNgCoords[index][0], hNgCoords[index][1])].hightlight(isHighlight);
+            squaresRef.current[getRefIndex(width, hNgCoords[index][0], hNgCoords[index][1])].hightlight(isHighlight);
         }
     }
 
@@ -256,7 +268,7 @@ const Board = forwardRef(function Board(props, inputRef) {
                     array[i][j] = Math.round(array[i][j]);
 
                     // Call refresh on the board square component
-                    ref.current[getRefIndex(width, i, j)].refresh(array[i][j]);
+                    squaresRef.current[getRefIndex(width, i, j)].refresh(array[i][j]);
                 }
             }
         }
@@ -272,7 +284,7 @@ const Board = forwardRef(function Board(props, inputRef) {
             for (var j = 0; j < width; j++) {
 
                 // Call refresh on the board square component
-                ref.current[getRefIndex(width, i, j)].refresh(array[i][j]);
+                squaresRef.current[getRefIndex(width, i, j)].refresh(array[i][j]);
             }
         }
     }
@@ -300,7 +312,7 @@ const Board = forwardRef(function Board(props, inputRef) {
         }
 
         // Refresh the board square component hinted to display the hint icon
-        ref.current[getRefIndex(width, mimeCoords[0], mimeCoords[1])].refresh(array[mimeCoords[0]][mimeCoords[1]]);
+        squaresRef.current[getRefIndex(width, mimeCoords[0], mimeCoords[1])].refresh(array[mimeCoords[0]][mimeCoords[1]]);
 
         // Return true to indicate we did not find a mime
         return true;
