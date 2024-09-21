411aeec4308f7d9c19b98478ed2deb473c1a5397
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-09-21 19:39:19 +0100

    Testing mobile issue on removing flags, causes reveal also

--- mimesWeep/src/logic/iosContextMenuHandler.js
+++ mimesWeep/src/logic/iosContextMenuHandler.js
@@ -21,22 +21,23 @@ export default class IOSContextMenuHandler {
 
     // On touch start we kick off a timer that will let us differentiate between a tap or a long-press
     onTouchStart = e => {
-        // Prevent any default IOS action, just as open share menu etc.
-        e.preventDefault();
+        console.warn("onTouchStart");
 
         this.longPressOccurred = false;
 
         // If timer runs out perform long-press action and flag we have done so
         this.longPressCountdown = setTimeout(() => {
             this.longPressOccurred = true;
+
+            console.log("rightClickCallback");
+
             this.rightClickCallback();
         }, longPressDurationMs);
     };
 
     // On move clear the timer
     onTouchMove = e => {
-        // Prevent any default IOS action, just as open share menu etc.
-        e.preventDefault();
+        console.warn("onTouchMove");
 
         clearTimeout(this.longPressCountdown);
         this.longPressOccurred = false;
@@ -44,8 +45,7 @@ export default class IOSContextMenuHandler {
 
     // On cancel clear the timer
     onTouchCancel = e => {
-        // Prevent any default IOS action, just as open share menu etc.
-        e.preventDefault();
+        console.warn("onTouchCancel");
 
         clearTimeout(this.longPressCountdown);
         this.longPressOccurred = false;
@@ -54,11 +54,13 @@ export default class IOSContextMenuHandler {
     // On touch end if long-press was not already triggered then perform tap action.
     // Regardless we clear the timeout and long-press flag.
     onTouchEnd = e => {
-        // Prevent any default IOS action, just as open share menu etc.
-        e.preventDefault();
+        console.warn("onTouchEnd");
 
         // User did not touch screen for long enough to be considered a long-press so perform tap action
         if (!this.longPressOccurred) {
+
+            console.warn("leftClickCallback");
+
             this.leftClickCallback();
         }
 
