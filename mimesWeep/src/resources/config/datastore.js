f071a25f3a76a5fe876025ff1f08ac05086ea5be
Sean Cox <seancox@Seans-MacBook-Pro-2.local>
2024-09-23 02:01:57 +0100

    change

--- mimesWeep/src/resources/config/datastore.js
+++ mimesWeep/src/resources/config/datastore.js
@@ -1,9 +1,11 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const settings = {
     API: {
         GraphQL: {
             endpoint: 'https://fha5mz4fmvgf7gx5wu6tcs4x7y.appsync-api.us-east-1.amazonaws.com/graphql',
             defaultAuthMode: 'apiKey',
-            apiKey: 'da2-sareqdfolbaxrm2hduryboysym',
+            apiKey: API_KEY,
             region: 'us-east-1',
         }
     }
